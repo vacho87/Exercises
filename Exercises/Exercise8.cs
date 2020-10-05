de4da4c74874d3a7d53c4e6d16edb1d8edc75f31
Vasily Kozhikhov <vacho.87@gmail.com>
2020-10-05 03:25:52 +0300

    Revert "Realized the idea with ExerciseChooser (missing realization of ExerciseChooser recursive mode)"
    This reverts commit 54477a2a6ce9b82ff053a851538fe3e73c58212a.

--- Exercises/Exercise8.cs
+++ Exercises/Exercise8.cs
@@ -8,12 +8,6 @@ namespace Exercises
 {
     class Exercise8 : Exercise
     {
-
-        /// <task_description>
-        /// 8.	Write a program that prints all prime numbers. 
-        /// (Note: if your programming language does not support arbitrary size numbers,
-        /// printing all primes up to the largest number you can easily represent is fine too.)
-        /// </task_description>
         public override void ExerciseRun()
         {
             for (ulong i=1; i< 18446744073709551615; i++ )
